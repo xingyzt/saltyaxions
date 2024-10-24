918cc4ad1a794ecaa061756c0cecc4604f1c3c13
Xing Liu <31298876+xingyzt@users.noreply.github.com>
2024-10-24 05:09:30 -0700

    Add files via upload

---
+++ reduce.py
@@ -0,0 +1,152 @@
+import mesa_reader as mr
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+import astropy.units as u
+import csv
+import os
+
+couplings = [
+    'g-7.0',
+    'g-7.5',
+    'g-7.75',
+    'g-8.0'
+]
+
+isotopes = [
+    'c12', 
+    'o16', 
+    'ne20', 
+    'na23',
+]
+
+labels = {
+    'model': 'slice',
+    'age': 'age (years)',
+    'til': 'time to core O depletion (years)',
+    'dt': 'dt (years)',
+    
+    'm': 'mass enclosed (Msun)',
+    'dm': 'dm (g)',
+    'r': 'r (Rsun)',
+    'dr': 'dr (cm)',
+    'T': 'T (K)',
+    'T_core': 'core T (K)',
+    'T_eff': 'effective T (K)',
+    'log_T': 'log T',
+    
+    'eps_grav': 'eps_grav (ergs/g s)',
+    'eps_nuc': 'eps_nuc (ergs/g s)', 
+    'eps_non_nuc_neu': 'eps_neu (ergs/g s)',
+    'eps_a': 'eps_a (ergs/g s)',
+    
+    'lum_gamma': 'lum_gamma (ergs/s)',
+    'lum_neu': 'lum_neu (ergs/s)',
+    'lum_a': 'lum_a (ergs/s)',
+    
+    'lum_gamma_surf': 'surface lum_gamma (ergs/s)',
+    'lum_neu_surf': 'surface lum_neu (ergs/s)',
+    'lum_a_surf': 'surface lum_a (ergs/s)',
+}
+
+for iso in isotopes:
+    labels[iso] = 'X_' + iso
+    labels['log_' + iso] = 'log X_' + iso
+    labels['X_' + iso] = 'avg X_' + iso
+
+def get_profile(i):
+
+    l = mr.MesaLogDir(coupling)
+    #bulk
+    b = mr.MesaData(f'{coupling}/profile{l.profile_numbers[i]}.data')
+
+    #header
+    h = b.header_data 
+
+    # print(h)
+
+    # profile
+    p = {
+        'profile': l.profile_numbers[i],
+        'model': l.model_numbers[i],
+        'age': h['star_age'],
+        'T_eff': h['Teff'],
+        
+        'T': b.data('T'), # Kelvins,
+        'log_T': b.data('logT'),
+        'dm': b.data('dm'), # g
+        'm': b.data('mass'), # M_sun
+        'dt': h['time_step'], # year
+        'r': b.data('radius'), # R_sun
+        'dr': (b.data('dr') * u.cm).to_value(u.R_sun),
+        'eps_grav': b.data('eps_grav'), # ergs / g s
+        'eps_nuc': b.data('eps_nuc'), 
+        # 'eps_nuc_minus_non_nuc_neu': b.data('eps_nuc_minus_non_nuc_neu'),
+        # 'eps_nuc_neu': b.data('eps_nuc_neu_total'),
+        # 'eps_nuc_plus_nuc_neu': b.data('eps_nuc_plus_nuc_neu'),
+        # 'eps_nuc_start': b.data('eps_nuc_start'),
+        'eps_non_nuc_neu': b.data('non_nuc_neu'),
+        'eps_a': b.data('axion'),
+        # 'net_nuclear_energy': 10**b.data('net_nuclear_energy'),
+        # 'net_energy': 10**b.data('net_energy'),
+        'lum': b.data('lum_erg_s')
+    }
+
+    for iso in isotopes:
+        p[ iso ] = b.data( iso )
+        p[ 'log_' + iso ] = np.nan_to_num(b.data( 'log_' + iso ), -99)
+
+        p[ 'X_' + iso ] = np.sum(p['dm'] * p[iso]) / np.sum(p['dm'])
+
+    p['lum_a'] = np.cumsum((p['eps_a'] * p['dm'])[::-1])[::-1]
+    p['lum_neu'] = np.cumsum((p['eps_non_nuc_neu'] * p['dm'])[::-1])[::-1]
+    p['lum_gamma'] = np.cumsum((p['eps_nuc'] * p['dm'])[::-1])[::-1]
+
+    p['lum_a_surf'] = p['lum_a'][-1]
+    p['lum_neu_surf'] = p['lum_neu'][-1]
+    p['lum_gamma_surf'] = p['lum_gamma'][-1]
+    p['T_core'] = p['T'][0]
+
+    return p
+
+for c in couplings:
+    coupling = c
+    l = mr.MesaLogDir(coupling)
+    N = len(l.profile_numbers)
+    # N = 2
+    profiles = list(ProcessPoolExecutor().map(get_profile, range(N)))
+
+    if not os.path.exists(f'csv/{coupling}'):
+        os.makedirs(f'csv/{coupling}')
+
+    for p in profiles:
+        p['til'] = profiles[N-1]['age'] - p['age']
+
+    with open(f'csv/{coupling}/index.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        p = profiles[0]
+        writer.writerow([ 
+            labels[key] 
+            for key in labels 
+            if np.isscalar(p[key])
+        ])
+        for p in profiles:
+            writer.writerow(np.array([ 
+                p[key]
+                for key in labels
+                if np.isscalar(p[key])
+            ]).T)
+
+    for p in profiles:
+
+        with open(f'csv/{coupling}/slice_{p["model"]}.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([ 
+                labels[key] 
+                for key in labels 
+                if not np.isscalar(p[key])
+            ])
+            writer.writerows(np.array([ 
+                p[key][::-1]
+                for key in labels
+                if not np.isscalar(p[key])
+            ]).T)
