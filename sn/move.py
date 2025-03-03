da7885da3865b5c47aadde7d60aa918da33dac7a
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2025-03-03 12:37:28 -0800

    Add galactic model

--- sn/move.py
+++ sn/move.py
@@ -1,11 +1,11 @@
 import os
 
 import shutil
-masses = [ 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ]
+masses = [ 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ]
 
 for m in masses:
     path = f'm{m:04.1f}_g-10.00_0'
     src = 'csv/' + path + '/index.csv'
-    dst = f'csv_simple_new/{m:.0f}.csv'
+    dst = f'csv_simple_new/{m:02.0f}.csv'
     shutil.copyfile(src, dst)
     
